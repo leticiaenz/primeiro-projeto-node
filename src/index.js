83de7c6eaefbcc264bf207c91a04e309e26e0fed
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-01-25 11:44:52 -0400

    Create middlewares

--- src/index.js
+++ src/index.js
@@ -7,13 +7,28 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
     const customersAlreadyreadyExists = customers.some((customer) => customer.cpf === cpf);
 
     if(customersAlreadyreadyExists) {
-        return response.status(400).json({ error: "Customer already exists"})
+        return response.status(400).json({ error: "Customer already exists"});
     }
 
     customers.push({
@@ -25,16 +40,11 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer) {
-        return response.status(400).json({error: "Customer not found"})
-    }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request; 
 
     return response.json(customer.statement);
+    
 });
 
 app.listen(3333);
