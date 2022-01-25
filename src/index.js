24b740f76046ebe50af077b4b6d004310e3a1b8f
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-01-25 11:02:15 -0400

    Validated account

--- src/index.js
+++ src/index.js
@@ -26,11 +26,15 @@ app.post("/account", (request, response) => {
 });
 
 app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
+    const { cpf } = request.headers;
 
     const customer = customers.find((customer) => customer.cpf === cpf);
 
-    return response.json(customers.statement);
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"})
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
